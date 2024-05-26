4c74063bc2c0d42b81d189f81997cc8ab9d438e3
Matt <matiasgbraconi@gmail.com>
2024-05-26 18:06:43 +0200

    models creado

--- routes/ataques.js
+++ routes/ataques.js
@@ -16,9 +16,43 @@ let router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-    const suma = 1 + 1;  
-    res.status(200).json({ message: suma });
+    const mensaje = "Este es el tablero";  
+    res.status(200).json({ message: mensaje });
   });
+
+/**
+ * @swagger
+ * /ataques/ataque:
+ *   patch:
+ *     summary: Este endpoint es de prueba!
+ *     description: La prueba fue exitosa.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               modelCoord:
+ *                 type: object
+ *                 properties:
+ *                   haveBase:
+ *                     type: boolean
+ *                     default: false
+ *                   isAttacked:
+ *                     type: boolean
+ *                     default: false
+ *     responses:
+ *       200:
+ *         description: Esto debe actualizar el tablero.
+ *       500:
+ *         description: Error de servidor.   
+ */
+
+router.patch('/ataque', function(req, res) {
+    const ataque = "Es un ataque";  
+    res.status(200).json({ message: ataque });
+}); 
 
 module.exports = router;
 
